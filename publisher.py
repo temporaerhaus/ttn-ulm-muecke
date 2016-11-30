70c185b2f3e3da518ff6c198bb20d3a28df6c69d
Matthias Schneider <matthias@beached.org>
2016-11-30 08:37:32 +0100

    changes

--- publisher.py
+++ publisher.py
@@ -9,9 +9,9 @@ import base64
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
-dev_eui = '0000000081F88297'
-app_eui = '70B3D57ED0001627'
-access_key = '7gf+WnmpNHipjcVEcVLJXNn31SxYJOpgrFhKPVJs8OM='
+dev_eui = '00000000AE57D1D3'
+app_eui = '70B3D57ED000146F'
+access_key = 'f6uPW0DYDdHCaPJgTByo0IwqWLcVK2EpAsdEKfAnl6c='
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -23,7 +23,7 @@ client.loop_start()
 
 while True:
     time.sleep(5)
-    msg = literal_eval('b"2508"')
+    msg = literal_eval('b"12345"')
     payload = json.dumps(dict(payload=base64.b64encode(msg).decode(), port=1, ttl='1h'))
     print(payload)
     client.publish(app_eui+'/devices/'+dev_eui+'/down', payload)
