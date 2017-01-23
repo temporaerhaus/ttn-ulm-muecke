e85089c4de98b2cc2d2b5352e5d8be960116538f
Matthias Schneider <matthias@beached.org>
2017-01-23 21:53:08 +0100

    more publisher code, but this will not work on TTN because readonly :)

--- publisher.py
+++ publisher.py
@@ -2,28 +2,28 @@
 import paho.mqtt.client as mqtt
 import time
 import json
-from ast import literal_eval
 import base64
-
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
-dev_eui = '00000000AE57D1D3'
-app_eui = '70B3D57ED000146F'
-access_key = 'f6uPW0DYDdHCaPJgTByo0IwqWLcVK2EpAsdEKfAnl6c='
+dev_eui = 'client1'
+app_id = 'flrp-mqtt'
+access_key = 'ttn-account-v2.yhZqDItky9xTY8DPgqKJpcSWVtHfIeTs9Fy6txl_8Hg'
 
 client = mqtt.Client()
 client.on_connect = on_connect
 
-client.username_pw_set(app_eui, access_key)
-client.connect('staging.thethingsnetwork.org', 1883, 60)
+client.username_pw_set(app_id, access_key)
+client.connect('eu.thethings.network', 1883, 60)
 
 client.loop_start()
 
 while True:
     time.sleep(5)
-    msg = literal_eval('b"12345"')
-    payload = json.dumps(dict(payload=base64.b64encode(msg).decode(), port=1, ttl='1h'))
-    print(payload)
-    client.publish(app_eui+'/devices/'+dev_eui+'/down', payload)
+    #msg = literal_eval('b"hallo welt"')
+    #payload = json.dumps(dict(payload=base64.b64encode(msg).decode(), port=1, ttl='1h'))
+    payload = json.dumps(dict(msg='flurp labs'))
+    channel = app_id + '/devices/' + dev_eui + '/down'
+    print(channel, payload)
+    client.publish(channel, payload)
