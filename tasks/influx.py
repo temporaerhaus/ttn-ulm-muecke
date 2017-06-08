faed5133fd9a95b0640f6a4a3d4f7e7c2b6e8a2f
Matthias Schneider <matthias@beached.org>
2017-06-08 21:38:01 +0200

    finished influxdb task

--- tasks/influx.py
+++ tasks/influx.py
@@ -14,29 +14,58 @@ class InfluxDBTask(DBTask):
 
     def save(self, app_id, mqtt_msg):
         self.logger.log('Executing Influx task...', 'TASK-INFLUX')
+
         json_raw = mqtt_msg.payload.decode("utf-8")
         data = json.loads(json_raw)
 
+        topic_splitted = mqtt_msg.topic.split('/')
+        dev_eui = topic_splitted[2]
+
+        best_rssi = -1000
+        best_gateway = ''
+        best_snr = -1000
+        best_received = ''
+        if 'gateways' in data['metadata']:
+            for gateway in data['metadata']['gateways']:
+                if gateway['rssi'] > best_rssi:
+                    best_rssi = gateway['rssi']
+                    best_gateway = gateway['gtw_id']
+                    best_snr = gateway.get('snr', 0)
+                    best_received = gateway['time']
+
+        self.logger.log('best_rssi: %d' % best_rssi)
+        self.logger.log('best_gateway: %s' % best_gateway)
+        self.logger.log('best_snr: %d' % best_snr)
+        self.logger.log('best_received: %s' % best_received)
+
+        if 'payload_fields' in data:
+            payload_fields = json.dumps(data['payload_fields'])
+        else:
+            payload_fields = None
+
         influx_json = [{
-            "measurement": "cpu_load_short",
-            "tags": {
-                "host": "",
+            'measurement': 'ttndata',
+            'tags': {
+                'dev_eui': str(dev_eui),
+                'app_id': int(app_id),
             },
-            "time": "2009-11-10T23:00:00Z",
-            "fields": {
-                "value": 0.64
+            'time': best_received,
+            'fields': {
+                'best_rssi': float(best_rssi),
+                'best_gateway': str(best_gateway),
+                'best_snr': float(best_snr),
+                'best_received': str(best_received),
+
+                'payload_fields': str(payload_fields),
+                'payload_raw': str(data['payload_raw']),
+
+                'gateways': json.dumps(data['metadata']['gateways']) if 'gateways' in data['metadata'] else ''
             }
-        }
-        ]
+        }]
 
-        device_eui = int(data['hardware_serial'], 16)
-
-        #client = InfluxDBClient('localhost', 8086, 'muecke', 'muecke', 'muecke')
-        #self.logger.log('Saving JSON payload to InfluxDB.')
-        #client.write_points(json_raw)
-
-        #result = client.query('select value from cpu_load_short;')
-        #print("Result: {0}".format(result))
+        client = InfluxDBClient('localhost', 8086, 'muecke', 'muecke', 'muecke')
+        self.logger.log('Saving JSON payload to InfluxDB.')
+        client.write_points(influx_json)
 
     def close(self):
         # TODO close influx connection?
