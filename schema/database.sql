97fa9e5e5cfca93a485af1424e45d639ba62d7ca
Matthias Schneider <matthias@beached.org>
2017-07-12 20:42:22 +0200

    removed old tables that are not used anymore

--- schema/database.sql
+++ schema/database.sql
@@ -1,42 +1,9 @@
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `password_reset` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `token` varchar(32) DEFAULT NULL,
-  `userid` int(11) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ix_password_reset_token` (`token`),
-  KEY `userid` (`userid`),
-  CONSTRAINT `password_reset_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `registration_confirm` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `token` varchar(32) DEFAULT NULL,
-  `userid` int(11) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ix_registration_confirm_token` (`token`),
-  KEY `userid` (`userid`),
-  CONSTRAINT `registration_confirm_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `apps` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userid` int(11) DEFAULT NULL,
   `app_id` varchar(120) NOT NULL,
   `app_key` varchar(120) NOT NULL,
-  `settings` text,
-  PRIMARY KEY (`id`),
-  KEY `userid` (`userid`),
-  CONSTRAINT `apps_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`)
+  `handler` VARCHAR(120) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `data` (
@@ -54,7 +21,7 @@ CREATE TABLE `data` (
   PRIMARY KEY (`id`),
   KEY `data_app_id_created_index` (`app_id`,`created`),
   KEY `gateway_eui` (`gateway_eui`),
-  CONSTRAINT `data_ibfk_1` FOREIGN KEY (`app_id`) REFERENCES `apps` (`id`)
+  CONSTRAINT `data_ibfk_1` FOREIGN KEY (`app_ id`) REFERENCES `apps` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `data_gateway` (
